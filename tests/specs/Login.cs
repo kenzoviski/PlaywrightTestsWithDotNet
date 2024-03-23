6886de7fd2cfe3be8df969f789c87dc6400a78ad
TA <tiago.armando@foodintech.pt>
2024-03-23 11:07:31 +0000

    Code optimization using SetUp (NUnit)

--- tests/specs/Login.cs
+++ tests/specs/Login.cs
@@ -8,6 +8,38 @@ namespace PlaywrightTests
     [TestFixture]
     public class Tests : PageTest
     {
+        #region Initialization
+        /*Before each test, basePageInstance and loginPageInstance are instantiated.
+        Also, it is guaranteed that before each test a new instance is created for browser and page.
+        */
+        private BasePage basePageInstance;
+        private LoginPage loginPageInstance;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            basePageInstance = new BasePage(Page);
+            loginPageInstance = new LoginPage(Page);
+            await Page.GotoAsync(basePageInstance.GetFullUrl("/login"));
+        }
+        #endregion
+
+        [Test]
+        public async Task LoginPageElements()
+        {
+            #region Gherkin
+            /*
+            Given the URL
+            When page is loaded
+            Then assert all the elements are loaded/visible on the page
+            */
+            #endregion
+
+            #region Test steps
+            await loginPageInstance.assertElementsLoginPage();
+            #endregion
+        }
+
         [Test]
         public async Task LoginTest()
         {
@@ -19,22 +51,11 @@ namespace PlaywrightTests
             */
             #endregion
 
-            #region Initialization
-            BasePage basePageInstance = new BasePage(Page);
-            LoginPage loginPageInstance = new LoginPage(Page);
-
-
-            await Page.GotoAsync(basePageInstance.GetFullUrl("/login"));
-            #endregion
-
-
             #region Test steps
-            await loginPageInstance.assertElementsLoginPage();
             await loginPageInstance.loginAction("test@test.com", "test");
             await loginPageInstance.assertHomePageHeader();
-
+            await loginPageInstance.assertPageTitle();
             #endregion
-
         }
     }
 }
