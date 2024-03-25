1ec7a4f74034d3052e2f4134873185b5a44dc971
TA <tiago.armando@foodintech.pt>
2024-03-25 13:10:31 +0000

    BasePage namespace

--- tests/pageObjets/BasePage.cs
+++ tests/pageObjets/BasePage.cs
@@ -2,46 +2,49 @@ using NUnit.Framework;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-public class BasePage
+namespace basePage
 {
-    #region Properties
-
-    protected readonly IPage Page;
-    protected readonly PlaywrightTest Assert;
-    protected static IPlaywright PlaywrightInstance;
-    protected static IBrowser BrowserInstance;
-
-    protected static string BaseUrl = "https://qa-automation-test-site.web.app";
-
-    #endregion
-
-    #region Constructors
-    /* Este construtor garante que o campo "Page" do objeto "BasePage" seja inicializado com o objeto "IPage"
-    e que um novo objeto PlaywrightTest seja criado e associado ao campo Assert.
-    */
-    public BasePage(IPage page)
+    public class BasePage
     {
-        Page = page;
-        Assert = new PlaywrightTest();
-    }
-    #endregion
+        #region Properties
 
-    [SetUp]
-    public void SetUp()
-    {
-        BrowserInstance = PlaywrightInstance.Chromium.LaunchAsync().GetAwaiter().GetResult();
-        PlaywrightInstance = Playwright.CreateAsync().GetAwaiter().GetResult();
-    }
+        protected readonly IPage Page;
+        protected readonly PlaywrightTest Assert;
+        protected static IPlaywright PlaywrightInstance;
+        protected static IBrowser BrowserInstance;
 
-    [TearDown]
-    public void TearDown()
-    {
-        BrowserInstance?.DisposeAsync();
-        PlaywrightInstance?.Dispose();
-    }
+        protected static string BaseUrl = "https://qa-automation-test-site.web.app";
 
-    public string GetFullUrl(string relativeUrl = "")
-    {
-        return string.IsNullOrEmpty(relativeUrl) ? BaseUrl : $"{BaseUrl}{relativeUrl}";
+        #endregion
+
+        #region Constructors
+        /* Este construtor garante que o campo "Page" do objeto "BasePage" seja inicializado com o objeto "IPage"
+        e que um novo objeto PlaywrightTest seja criado e associado ao campo Assert.
+        */
+        public BasePage(IPage page)
+        {
+            Page = page;
+            Assert = new PlaywrightTest();
+        }
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            BrowserInstance = PlaywrightInstance.Chromium.LaunchAsync().GetAwaiter().GetResult();
+            PlaywrightInstance = Playwright.CreateAsync().GetAwaiter().GetResult();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            BrowserInstance?.DisposeAsync();
+            PlaywrightInstance?.Dispose();
+        }
+
+        public string GetFullUrl(string relativeUrl = "")
+        {
+            return string.IsNullOrEmpty(relativeUrl) ? BaseUrl : $"{BaseUrl}{relativeUrl}";
+        }
     }
 }
